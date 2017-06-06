302d2e424e5ea491f2dcb7faa7a32535fb833653
omercse26 <omercse26@gmail.com>
2017-06-06 00:07:34 -0500

    basic executor main loop

--- Shell/main.cpp
+++ Shell/main.cpp
@@ -1,10 +1,74 @@
 #include <cstdio>
 #include <iostream>
-using namespace std;
+#include <vector>
+
+void printJobs() 
+{
+	std::cout << "Printed Jobs" << std::endl;
+}
+
+int isBuiltInCommand(const std::string& command)
+{
+	if (command == "fg") {
+		return 0;
+	}
+	else if (command == "bg") {
+		return 1;
+	}
+	else if (command == "kill") {
+		return 2;
+	}
+	else {
+		return -1;
+	}
+}
+
+std::vector<std::string> splitCmd(const std::string& command)
+{
+	std::vector<std::string> subCmds;
+	size_t start = 0;
+	while (true) {
+		auto index = command.find(' ', start);
+		if (index == std::string::npos) {
+			std::string last = command.substr(start);
+			auto amp = last.find('&');
+			if (amp != std::string::npos) {
+				
+			}
+			else {
+				subCmds.push_back(last);
+			}
+			break;
+		}
+		subCmds.push_back(command.substr(start, index));
+		start = index;
+	}
+
+	return subCmds;
+}
+
+void executeCommand(const std::string& command)
+{
+	if (command == "jobs") {
+		printJobs();
+		return;
+	}
+
+	std::vector<std::string> subCmds = splitCmd(command);
+	int builtinCommand = isBuiltInCommand(subCmds[0]);
+
+	if (builtinCommand != -1) {
+
+	}
+}
 
 int main()
 {
-    printf("hello from Shell!\n");
-	cin.ignore();
+	std::string command;
+	while (true) {
+		std::cout << "$:";
+		std::cin >> command;
+		executeCommand(command);
+	}
     return 0;
 }
